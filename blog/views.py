22933a875d4550192c431cec9fcd3e5f153cc168
lr131 <lr131k@gmail.com>
2016-02-01 05:23:10 +0800

    add form

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.utils import timezone
+from blog.forms import PostForm
 from .models import Post
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    if request.method == "POST":
+                form = PostForm(request.POST)
+                if form.is_valid():
+                    post = form.save(commit=False)
+                    post.author = request.user
+                    post.published_date = timezone.now()
+                    post.save()
+                    return render(request, 'blog/post_list.html', {'form': form, 'posts': posts})
+                    #return redirect(request, 'blog/post_list.html', {'form': form, 'posts': posts})
+    else:
+                form = PostForm()
+    return render(request, 'blog/post_list.html', {'form': form, 'posts': posts})
